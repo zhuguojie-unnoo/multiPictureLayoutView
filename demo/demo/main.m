423d1aec39172c4c6c797db6e960b696137289a4
zhuguojie-unnoo <3208580367@qq.com>
2016-01-18 09:23:15 +0800

    多图控件

---
+++ demo/demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo
+//
+//  Created by ios_zhu on 16/1/5.
+//  Copyright © 2016年 ios_zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
